5f362a6114229b92f7b692e11bbf487c063b2107
Michael Walker <michael.walker@softwire.com>
2017-11-28 16:18:21 +0000

    TT-1681: Add new Event Constant for pending idp authn.
    Authors: @michaelwalker, @mahmudh

--- rest-utils/src/main/java/uk/gov/ida/eventsink/EventSinkHubEventConstants.java
+++ rest-utils/src/main/java/uk/gov/ida/eventsink/EventSinkHubEventConstants.java
@@ -15,6 +15,7 @@ public interface EventSinkHubEventConstants {
         String IDP_AUTHN_SUCCEEDED = "idp_authn_succeeded";
         String NO_AUTHN_CONTEXT = "no_authn_context";
         String IDP_AUTHN_FAILED = "idp_authn_failed";
+        String IDP_AUTHN_PENDING = "idp_authn_pending";
         String CYCLE01_MATCH = "cycle01_match";
         String CYCLE01_NO_MATCH = "cycle01_no_match";
         String WAITING_FOR_CYCLE3_ATTRIBUTES = "waiting_for_cycle3_attributes";
